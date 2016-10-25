2551a6216ad6ff6014168696fd29aa904eeefaca
Yashna Kumar <yashnak@wirelessprv-10-193-124-175.near.illinois.edu>
2016-10-25 01:42:39 -0500

    Controller Implementation

--- poll.py
+++ poll.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
 
-poll_data =
- [{'question' : 'Do you like Whiskey and Coke?',
-   'fields'   : ['Yes', 'No', 'Unfamiliar with Drink']},
+ANS_ENUM = ['No', 'Unfamiliar with Drink', 'Yes']
+response_data = ['You should try bacardi and coke', 'You should try vodka martini', 'You should try mimosa']
 
-   {'question' : 'Do you like Long Island?',
-   'fields'   : ['Yes', 'No', 'Unfamiliar with Drink']},
 
-   {'question' : 'Do you like Gin and Tonic?',
-   'fields'   : ['Yes', 'No', 'Unfamiliar with Drink']}]
+poll_data = {'question' : 'Do you like Whiskey and Coke?',
+   'fields'   : ['Yes', 'No', 'Unfamiliar with Drink']}
+
 filename = 'data.txt'
+
+def response(val):
+    return response_data[val]
 
 @app.route('/')
 def root():
@@ -20,12 +21,22 @@ def root():
 @app.route('/poll')
 def poll():
     vote = request.args.get('field')
+    vote_val = -1; 
+    for val in range(0, 3):
+        if vote == ANS_ENUM[val]:
+            vote_val = val
+            print(vote_val)
+    if vote_val == -1:
+        print("Error")
+            
+    str_response = response(vote_val)          
 
     out = open(filename, 'a')
-    out.write( vote + '\n' )
+    out.write( str(vote_val) + '\n' )
     out.close()
 
-    return render_template('thankyou.html', data=poll_data)
+    return render_template('thankyou.html', data=str_response)
+
 
 @app.route('/results')
 def show_results():
